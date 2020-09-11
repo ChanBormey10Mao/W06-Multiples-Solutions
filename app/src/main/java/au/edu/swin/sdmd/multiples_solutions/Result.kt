30377e3875367f1fdb46cefe0657c8399d1829ef
N. A. Ronald <nronald@swin.edu.au>
2020-09-11 13:37:15 +1000

    End of first video.

---
+++ app/src/main/java/au/edu/swin/sdmd/multiples_solutions/Result.kt
@@ -0,0 +1,8 @@
+package au.edu.swin.sdmd.multiples_solutions
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Result(val opResult: Int): Parcelable {
+}
---
+++ app/src/main/java/au/edu/swin/sdmd/multiples_solutions/ResultActivity.kt
@@ -0,0 +1,17 @@
+package au.edu.swin.sdmd.multiples_solutions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val result = intent.getParcelableExtra<Result>("result")
+        val tvResult = findViewById<TextView>(R.id.result)
+        tvResult.text = result?.opResult.toString()
+
+    }
+}
